96f350a95e19f711deca22522607b4fb94caed7f
Evelina Simion <evelinasimion39@gmail.com>
2020-08-06 20:34:14 +0300

    Done

--- Week4/HomeWork/HWweek4course2.js
+++ Week4/HomeWork/HWweek4course2.js
@@ -24,10 +24,13 @@ function string_to_array(myVar) {
 }
 console.log(string_to_array("Robin Singh"));
 
-//EX4 - mai gandeste-te!!!!!!
+//EX4 
 function abbrev_name(myVar) {
-
+    var cutName = myVar.split(" "); // Splits the words till space
+    var endName;
+    return endName = cutName[0] + " " + cutName[1].charAt(0) + "."; // Takes teh first word and addd space and add the fisrt character form the second word and a dott
 }
+console.log(abbrev_name("Robin Singh"));
 
 //EX5
 function capitalize(myVar) {
@@ -44,14 +47,38 @@ function truncate_string(myVar, specifiedNumber) {
 console.log(truncate_string("Robin Singh", 4));
 
 //EX7
-function isUpperCaseAt (myVar, position) {
+function isUpperCaseAt(myVar, position) {
     return myVar.charAt(position).toUpperCase == myVar.charAt(position); // - Compare if the character at the position has an UpperCase with himself
 }
 console.log(isUpperCaseAt('Js STRING EXERCISES', 1));
 
 //EX8
-function insert (myVar, myWord, position) {
-    var strSpace= " ";
+function insert(myVar, myWord, position) {
+    var strSpace = " ";
     return myVar.slice(0, position) + myWord + strSpace + myVar.slice(position); // - Goes from 0 to the specified position, adds the word and a space and pulls it back together with the rest
 }
-console.log(insert('We are doing some exercises',  'Javascript' , 18));
+console.log(insert('We are doing some exercises', 'Javascript', 18));
+
+//EX9 - something doesn't work
+function remove_first_occurance(myVar, word) {
+    var positionWord = myVar.indexOf(word);   // Finds out the position of word
+    return myVar.slice(0, positionWord) + myVar.slice(positionWord, myVar.length);
+}
+console.log(remove_first_occurance("The quick brown fox jumps over the lazy dog", 'the'));
+
+//EX10
+function compare_strings(String1, String2) {
+    var string1LowerCase = String1.toLowerCase(); // First string lower case
+    var string2LowerCase = String2.toLowerCase(); // Second string lower case
+    var Egal = string1LowerCase == string2LowerCase; // Compare
+    return Egal;
+}
+console.log(compare_strings('abcd', 'AbcD'));
+
+//EX11
+function Uncapitalize(myVar) {
+    //return myVar.charAt(0).toLowerCase(); - First letter uncapitalized
+    //return myVar.slice(1); - Shows the string without the first letter
+    return myVar.charAt(0).toLowerCase() + myVar.slice(1); // - Back together
+}
+console.log(Uncapitalize('Js string exercises'));
