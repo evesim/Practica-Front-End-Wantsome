34ccd8c85656fa4ece8e51759aaa47300c689107
Evelina Simion <evelinasimion39@gmail.com>
2020-08-22 10:39:26 +0300

    Practice

--- Week7/Practice/arguments.js
+++ Week7/Practice/arguments.js
@@ -1,33 +1,62 @@
 /*  
     param1: nr
     param2: nr
-    param3: S (scadere), A(adunare), I(inmultire), D(impartire)
+    param3: S sau "scadere", A sau "adunare", I sau "inmultire", D sau "impartire"
 
     functia intoarce rezultatul
 */
 
+
+var substractOperation = ["S", "subtract", "SS", "SSS","sub"];
+var sumOperation = ["A", "adunare"];
+var multiplyOperation = ["I", "inmultire"];
+var devideOperation = ["D", "imparire"];
+
 function calculator(number1, number2, mathOperation) {
-    var result;
-    switch (mathOperation) {
-        case "S":
-            result = number1 - number2;
-            break;
-        case "A":
-            result = number1 + number2;
-            break;
-        case "I":
-            result = number1 * number2;
-            break;
-        case "D":
-            result = number1 / number2;
-            break;
-        default:
-            console.log("Not valid. Try again!")
-            break;
-    }
+    var result; 
+    var unifyMathOperation = detectMathOperation(mathOperation); //detect what kind of operation we need to do
+    result = unifyMathOperation(number1,number2);
+    /*
+        var unifyMathOperation = substract();
+    */
     return result;
 }
-calculator(4, 2, "S"); //2
-calculator(4, 2, "A"); //6
-calculator(4, 2, "I"); //8
-calculator(4, 2, "D"); //2
+
+function detectMathOperation(mathOperation){ //return the funtion
+    if (substractOperation.includes(mathOperation)){
+        return substract; //function from lower
+    }else if (sumOperation.includes(mathOperation)){
+        return sum;
+    }else if (multiplyOperation.includes(mathOperation)){
+        return multiply;
+    }else if (devideOperation.includes(mathOperation)){
+        return devide;
+    }
+}
+
+function sum(number1, number2) { //function for operation
+    return number1 + number2;
+}
+
+function substract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function devide(number1, number2) {
+    return number1 / number2;
+}
+
+console.log(calculator(5,2,"inmultire"));
+console.log(calculator(6,3,"devide"));
+
+/*console.log(calculator(4, 2, "S")); //2
+console.log(calculator(4, 2, "scadere")); //2
+
+console.log(calculator(4, 2, "A")); //6
+console.log(calculator(4, 2, "I")); //8
+console.log(calculator(4, 2, "D")); //2
+console.log(calculator(4, 2, "H")); //not */
