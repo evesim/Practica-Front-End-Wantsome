645cec0e3cfeccd8c9e8f061eaf69385886794f0
Evelina Simion <evelinasimion39@gmail.com>
2020-08-22 10:46:34 +0300

    Practice

--- Week7/Practice/arguments.js
+++ Week7/Practice/arguments.js
@@ -7,31 +7,31 @@
 */
 
 
-var substractOperation = ["S", "subtract", "SS", "SSS","sub"];
+var substractOperation = ["S", "subtract", "SS", "SSS", "sub"];
 var sumOperation = ["A", "adunare"];
 var multiplyOperation = ["I", "inmultire"];
-var devideOperation = ["D", "imparire"];
+var devideOperation = ["D", "imparire","devide"];
 
 function calculator(number1, number2, mathOperation) {
-    var result; 
     var unifyMathOperation = detectMathOperation(mathOperation); //detect what kind of operation we need to do
-    result = unifyMathOperation(number1,number2);
-    /*
-        var unifyMathOperation = substract();
-    */
-    return result;
+    if (type off unifyMathOperation === "function") {
+        return unifyMathOperation(number1, number2);
+    }else {
+        return "Operatia nu este declarata";
+    }
 }
 
-function detectMathOperation(mathOperation){ //return the funtion
-    if (substractOperation.includes(mathOperation)){
+function detectMathOperation(mathOperation) { //return the funtion
+    if (substractOperation.includes(mathOperation)) {
         return substract; //function from lower
-    }else if (sumOperation.includes(mathOperation)){
+    } else if (sumOperation.includes(mathOperation)) {
         return sum;
-    }else if (multiplyOperation.includes(mathOperation)){
+    } else if (multiplyOperation.includes(mathOperation)) {
         return multiply;
-    }else if (devideOperation.includes(mathOperation)){
-        return devide;
+    } else if (devideOperation.includes(mathOperation)) {
+        return divide;
     }
+    return null;
 }
 
 function sum(number1, number2) { //function for operation
@@ -46,12 +46,12 @@ function multiply(number1, number2) {
     return number1 * number2;
 }
 
-function devide(number1, number2) {
+function divide(number1, number2) {
     return number1 / number2;
 }
 
-console.log(calculator(5,2,"inmultire"));
-console.log(calculator(6,3,"devide"));
+console.log(calculator(5, 2, "inmultire"));
+console.log(calculator(6, 3, "devide"));
 
 /*console.log(calculator(4, 2, "S")); //2
 console.log(calculator(4, 2, "scadere")); //2
