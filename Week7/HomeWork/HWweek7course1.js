c671a47b620978f0974c008914b0c91de0d97f3a
Evelina Simion <evelinasimion39@gmail.com>
2020-08-23 12:54:59 +0300

    Done

--- Week7/HomeWork/HWweek7course1.js
+++ Week7/HomeWork/HWweek7course1.js
@@ -2,46 +2,66 @@
 //raporta acest lucru in consola. (exemplu: “2 este numar par”);
 //Scrieti functia in doua variante: while si for
 
-//loop function - for which will go from 0 to 20 with i being incremented +1
-for (i = 0; i <= 20; i++) {
-    //if the rest from the i%2 is 0 the will print (show in console) "i (- being the numar that will be between 0 and 20) este numar par"
-    if (i % 2 === 0) {
-        console.log(i + " este numar par");
+//declare a named function
+function evenOrNot() {
+    //loop function - for which will go from 0 to 20 with i being incremented +1
+    for (i = 0; i <= 20; i++) {
+        //if the rest from the i%2 is 0 the will print (show in console) "i (- being the numar that will be between 0 and 20) este numar par" else "is impar"
+        if (i % 2 === 0) {
+            console.log(i + " este numar par");
+        } else {
+            console.log(i + " este numar impar");
+        }
     }
 }
 
+evenOrNot();
 
-//declare i
-var i = 0;
-//loop function- while i is smaller or equal to 20 
-while (i <= 20) {
-    //if the rest from the i%2 is 0 the will print (show in console) "i (- being the numar that will be between 0 and 20) este numar par"
-    if (i % 2 === 0) {
-        console.log(i + " este numar par");
+//declare a named function
+function evenOrNot() {
+    //declare i
+    var i = 0;
+    //loop function- while i is smaller or equal to 20 
+    while (i <= 20) {
+        //if the rest from the i%2 is 0 the will print (show in console) "i (- being the numar that will be between 0 and 20) este numar par" else "is impar"
+        if (i % 2 === 0) {
+            console.log(i + " este numar par");
+        } else {
+            console.log(i + " este numar impar");
+        }
+        //incremnet i with +1
+        i++;
     }
-    //incremnet i with +1
-    i++;
 }
+evenOrNot();
 
 //2. Scrieti un for care itereaza de la 0 la 10. Pentru fiecare iteratie, se va multiplica numarul curent cu 9 si se va afisa
 //rezultatul. (exemplu: “3*9=27”).
 //Scrieti functia in doua variante: while si for
 
-//loop function - for which will go from 0 to 10 with i being incremented +1
-for (i = 0; i <= 10; i++) {
-    //print multiplication of i with 9
-    console.log(i + "*9=" + (i * 9));
+//declare a named function
+function multiply() {
+    //loop function - for which will go from 0 to 10 with i being incremented +1
+    for (i = 0; i <= 10; i++) {
+        //print multiplication of i with 9
+        console.log(i + "*9=" + (i * 9));
+    }
 }
+multiply();
 
-//declare i
-var i = 0;
-//loop function- while i is smaller or equal to 20 
-while (i <= 10) {
-    //print multiplication of i with 9
-    console.log(i + "*9=" + (i * 9));
-    //increment i with +1 
-    i++;
+//declare a named function
+function multiply() {
+    //declare i
+    var i = 0;
+    //loop function- while i is smaller or equal to 20 
+    while (i <= 10) {
+        //print multiplication of i with 9
+        console.log(i + "*9=" + (i * 9));
+        //increment i with +1 
+        i++;
+    }
 }
+multiply();
 
 //3. Pentru fiecare cifra de la 1 la 10, afisati tabla inmultirii.
 //(exemplu: 1 * 0 = 0
@@ -50,16 +70,21 @@ while (i <= 10) {
 //    1 * 10 = 10 )
 //   Pentru a afisa in consola pe linie noua puteti concatena un string, la final, cu + “\n”
 
-//loop function - for which will go from 1 to 10 with i being incremented +1
-for (i = 1; i <= 10; i++) {
-    ////loop function - for which will go from 1 to 10 with j being incremented +1
-    for (j = 1; j <= 10; j++) {
-        //multiplication between i and j
-        result = i * j;
-        //print the multiplication under the format i*j=(i*j)
-        console.log(i + "*" + j + "=" + result + "\n");
+//declare a named function
+function multiplicationTable() {
+    //loop function - for which will go from 1 to 10 with i being incremented +1
+    for (i = 1; i <= 10; i++) {
+        ////loop function - for which will go from 1 to 10 with j being incremented +1
+        for (j = 1; j <= 10; j++) {
+            //multiplication between i and j
+            result = i * j;
+            //print the multiplication under the format i*j=(i*j)
+            console.log(i + "*" + j + "=" + result + "\n");
+        }
     }
 }
+
+multiplicationTable()
 
 //4. Pentru functia dezvoltata la cerinta 2 de la tema anterioara, verificati si afisati in consola fiecare calificativ care se
 //poate obtine pentru punctajele incepand de 1 la 10. (exemplu: “Pentru 2, obtii calificativul E. \n Pentru 7, obtii
@@ -74,7 +99,10 @@ for (i = 1; i <= 10; i++) {
 //- 9 = A
 //- 10 = A+
 //Functia va returna un text de forma "Calificatul corespunzator punctajului [punctaj] este [calificativ]".
+
+//declare an expression function with argument "grade"
 var calificativ = function (grade) {
+    //if the grades are smaller or equal with 3,6,8,9,10 will return a specific string
     if (grade <= 3) {
         return "Calificatul corespunzator punctajului " + grade + " este " + "E";
     } else if (grade <= 6) {
@@ -87,4 +115,13 @@ var calificativ = function (grade) {
         return "Calificatul corespunzator punctajului " + grade + " este " + "A+"
     }
 }
-console.log(calificativ(5));
+
+//declare named function
+function verifCalif() {
+    ////loop function - for which will go from 1 to 10 with i being incremented +1
+    for (i = 1; i <= 10; i++) {
+        //will called the function and print it
+        console.log(calificativ(i))
+    }
+}
+verifCalif();
