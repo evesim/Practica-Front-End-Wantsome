5644e8a2eb2a0c41945f5ea1dfb5ba842bc20234
Evelina Simion <evelinasimion39@gmail.com>
2020-09-18 19:49:47 +0300

    Done

---
+++ Week10/Practice/js/index.js
@@ -0,0 +1,29 @@
+// Change the value of an element
+const onInputChange = () => {
+    const userWelcomeE1 = document.getElementById("userWelcome");
+    const userNameE1 = document.querySelector("#userName");
+
+    userWelcomeE1.textContent = `Welcome ${userNameE1.value}`;
+}
+
+const onLogOut = () => {
+    const userWelcomeE1 = document.getElementById("userWelcome");
+    userWelcomeE1.textContent = `Salut`;
+}
+
+// Will change when you hover over
+const onElementMouseOver = () => {
+    const userWelcomeE1 = document.getElementById("userWelcome");
+    userWelcomeE1.classList.add("over");
+}
+
+const onElementMouseOut = () => {
+    const userWelcomeE1 = document.getElementById("userWelcome");
+    userWelcomeE1.classList.remove("over");
+}
+
+const onPageLoad = () => {
+    console.log(document.getElementById("userWelcome"));
+}
+
+onLogOut();
