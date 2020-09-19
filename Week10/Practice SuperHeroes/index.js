e11cde388c23e10e2d5ef7d4736af5ba0647a2a0
Evelina Simion <evelinasimion39@gmail.com>
2020-09-19 13:22:34 +0300

    Done

--- Week10/Practice SuperHeroes/index.js
+++ Week10/Practice SuperHeroes/index.js
@@ -2,52 +2,54 @@ const xhttp = new XMLHttpRequest();
 
 const populateHeader = (info) => {
     const domH1 = document.createElement("h1");
-    const domH2 = document.createElement("h2");
-    const headerE1 = document.getElementById("header");
+    const domH2 = document.createElement("p");
+    const headerEl = document.getElementById("header");
 
-    domH1.innerHTML = info.squadName;
-    domH2.innerHTML = `Hometown: ${info.homeTown} // Formed: ${info.formed}`;
+    domH1.innerText = info.squadName;
+    domH1.id = "IDTEST"
+    domH2.innerText = `Hometown: ${info.homeTown} // Formed: ${info.formed}`;
 
-    headerE1.appendChild(domH1);
-    headerE1.appendChild(domH2);
+    headerEl.appendChild(domH1);
+    headerEl.appendChild(domH2);
+
 }
 
-
-
-const populateContent = (info) => {
+const populateContent = (members) => {
     members.forEach(member => {
+        const divEl = document.createElement("div");
         const domH1 = document.createElement("h2");
         const p1 = document.createElement("p");
         const p2 = document.createElement("p");
         const ulEl = document.createElement("ul");
-        const contentEl = document.getElementById("content")
+        const contentEl = document.getElementById("content");
 
+        divEl.classList.add("superheroItem");
         domH1.innerText = member.name;
-        p1.innerText = `Secret Identity: ${member.secretIdentity}`;
+        p1.innerText = `Secret identity: ${member.secretIdentity}`;
         p2.innerText = `Age: ${member.age}`;
-        ulEl.innerText = "Superpower: ";
-
+        ulEl.innerText = "Superpowers: ";
+        
         member.powers.forEach(power => {
-            const liEl = document.createElement("li");
-            liEl.innerText = power;
-            ulEl.appendChild(liEl);
-        });
+            const liEL = document.createElement("li");
+            liEL.innerText = power;
+            ulEl.appendChild(liEL);
+        })
 
-        contentEl.appendChild(domH1);
-        contentEl.appendChild(p2);
-        contentEl.appendChild(ulEl);
-        contentEl.appendChild(p1);
-    })
+        divEl.appendChild(domH1);
+        divEl.appendChild(p2);
+        divEl.appendChild(ulEl);
+        divEl.appendChild(p1);
+        contentEl.appendChild(divEl);
+    });
 }
 
 xhttp.onreadystatechange = () => {
-    if (xhttp.readyState === 4 && xhttp.status === 200) {
+    if(xhttp.readyState === 4 && xhttp.status === 200) {
         const responseToJSON = JSON.parse(xhttp.responseText);
         populateHeader(responseToJSON);
         populateContent(responseToJSON.members);
     }
 }
-xhttp.open("GET", "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json", true);
-//xhttp.open("GET", "./example.json", true);
+//xhttp.open("GET", "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json", true);
+xhttp.open("GET", "./example.json", true);
 xhttp.send();
-
